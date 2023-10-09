53fdcf520b1d943be019e785177ce7f479fb66bf
xiaoye_hua <huag@kth.se>
2023-10-09 20:06:39 +0200

    1st commit

---
+++ seup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='package'
+    , version='0.0.1'
+    , author='Hua'
+    , packages=['package']
+    , zip_safe=False
+)
---
+++ src/FeatureCreator/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @Filename:    __init__.py.py
+# @Time:        09/10/2023 20:01
+# @Desc:
+
+
+class FeatureCreator:
+    def __init__(self):
+        a = 3
+        c = 4
